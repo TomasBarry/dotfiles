903ab3576797411acd46e973a08e4541402ed751
Tomas Barry <tomas.barry@hotmail.com>
2016-09-28 14:47:32 +0100

    Removed sudo's

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-get update
+apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 cat > /etc/apt/sources.list.d/docker.list << EOF
 deb https://apt.dockerproject.org/repo ubuntu-trusty main
 EOF
 
-sudo apt-get update
-sudo apt-get purge lxc-docker
+apt-get update
+apt-get purge lxc-docker
 apt-cache policy docker-engine
 
-sudo apt-get update
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get update
+apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
-sudo apt-get update
-sudo apt-get install docker-engine
-sudo service docker start
-sudo docker run hello-world
-sudo apt-get upgrade docker-engine
+apt-get update
+apt-get install docker-engine
+service docker start
+docker run hello-world
+apt-get upgrade docker-engine
