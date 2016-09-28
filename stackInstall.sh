ba6721e9099824ea79bd26ce1125f4a57e72fa7c
Tomas Barry <tomas.barry@hotmail.com>
2016-09-28 12:12:44 +0100

    Update the PATH for Stack

--- stackInstall.sh
+++ stackInstall.sh
@@ -5,5 +5,9 @@
 mkdir ~/home/barryto/.local/bin
 
 
+# Update the PATH variable
+export PATH="$PATH:/home/barryto/.local/bin"
+
+
 # Download and install stack
 curl -sSL https://get.haskellstack.org/ | sh
