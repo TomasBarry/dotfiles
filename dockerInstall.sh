f056dc242276d3e0b0ca0b0117dd2af8a58d5b1f
Tomas Barry <tomas.barry@hotmail.com>
2016-09-28 14:15:11 +0100

    Fixed permission issues

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-cat > /etc/apt/sources.list.d/docker.list << EOF
+sudo cat > /etc/apt/sources.list.d/docker.list << EOF
 deb https://apt.dockerproject.org/repo ubuntu-trusty main
 EOF
 
