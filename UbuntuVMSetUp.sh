9e64a231ea39720c1601b4468f6764c86c68c6a9
Tomas Barry <tomas.barry@hotmail.com>
2016-09-28 11:19:39 +0100

    Create UbuntuVMSetUp.sh
    Sets the right resolution, downloads sublime and sets up the .bash_profile

---
+++ UbuntuVMSetUp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# This file sets up a VM on a Dell XPS 13 to custom preferences
+# This file can be run after the VM is created
+
+
+# Set the screen scale factor as the Dell XPS 13 has an extremely high resolution
+# resulting in everything being tiny in the VM view
+dconf write /com/ubuntu/user-interface/scale-factor "{'Virtual1': 14}"
+
+
+# Download and set up Sublime Text 3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+
+# Set up the .bash_profile
+echo "
+alias sublime='~/opt/sublime_text/sublime_text'
+" >> ~/.bash_profile
+
+
+# Execute the .bash_profile
+source ~/.bash_profile
