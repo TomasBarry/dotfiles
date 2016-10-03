616583f7a2ea16e6cc0299c05138a1b4d84560da
Tomas Barry <tomas.barry@hotmail.com>
2016-10-03 12:34:39 -0700

    Set git globals for my own profile

---
+++ gitEnvVariablesSetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Set the git user name and email for the computer
+
+echo '########################################################################'
+echo '################## Setting Git globals #################################'
+echo ''
+
+git config --global user.email "tomas.barry@hotmail.com"
+git config --global user.name "Tomas Barry"
+
+echo ''
+echo '################### Set Git globals ####################################'
+echo '########################################################################'
