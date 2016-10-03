5c00d3a687e1aaae84695778f782bf9b54b1ad32
Tomas Barry <tomas.barry@hotmail.com>
2016-10-03 10:43:45 -0700

    Updated documentation of shell scripts

--- UbuntuVMSetUp.sh
+++ UbuntuVMSetUp.sh
@@ -5,21 +5,81 @@
 # This file can be run after the VM is created
 
 
+# Globals
+
+# Directory containing this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Array of possible text colours
+declare -a colours=('black' 'blue' 'green' 'cyan' 'red' 'magenta' 'yellow');
+
+# Counter for text colour switching
+i=0
+
+
+# Helper Functions
+
+# Change the colour of the text output to the screen
+function changeColour() {
+	setterm -term linux -fore $colours[$i % ${#colours[@]}]
+	i = i + 1
+}
+
+
 # Set the screen scale factor as the Dell XPS 13 has an extremely high resolution
 # resulting in everything being tiny in the VM view
+echo '##########################################################################'
+echo '##################### Setting screen resolution ##########################'
+echo ''
+
+changeColour()
 dconf write /com/ubuntu/user-interface/scale-factor "{'Virtual1': 14}"
+
+echo ''
+echo '##################### Screen resolution set ##############################'
+echo '##########################################################################'
 
 
 # Set the keyboard layout to English UK
+echo '##########################################################################'
+echo '#################### Setting keyboard layout #############################'
+echo ''
+
+changeColour()
 setxkbmap -layout gb
+
+echo ''
+echo '################### Set keyboard layout #################################'
+echo '##########################################################################'
 
 
 # Download and set up Sublime Text 3
+echo '##########################################################################'
+echo '################## Installing Sublime Text ###############################'
+echo ''
+
+changeColour()
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
+sudo apt-get update
+
+echo ''
+echo '################# Sublime Text Installed #################################'
+echo '##########################################################################'
 
 
 # Set up the .bash_profile
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo '##########################################################################'
+echo '################# Bash set up ############################################'
+echo ''
+
+changeColour()
 mv $DIR/.bash_profile ~/.bash_profile
+
+echo ''
+echo '################# Bash is set up #########################################'
+echo '##########################################################################'
+
+
+setterm -term linux -fore 'white'
--- chmodShellScripts.sh
+++ chmodShellScripts.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+
+# This file changes the mode of every shell script in the directory
+# containing this file (recursively)
+# Files that end in .sh are changed to u+x meaning that they are
+# executable by the current user
+
+# Globals
+
+# The directory containing this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-chmod u+x $DIR/*.sh
+
+
+chmod u+x $DIR/**/*.sh
