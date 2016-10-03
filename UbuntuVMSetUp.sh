8798576c27dbfcb618ba4502cf0647fe785af8f0
Tomas Barry <tomas.barry@hotmail.com>
2016-10-03 12:30:32 -0700

    Removed colour switches

--- UbuntuVMSetUp.sh
+++ UbuntuVMSetUp.sh
@@ -10,21 +10,6 @@
 # Directory containing this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Array of possible text colours
-declare -a colours=('black' 'blue' 'green' 'cyan' 'red' 'magenta' 'yellow');
-
-# Counter for text colour switching
-i=0
-
-
-# Helper Functions
-
-# Change the colour of the text output to the screen
-function changeColour() {
-	setterm -term linux -fore $colours[$i % ${#colours[@]}]
-	i = i + 1
-}
-
 
 # Set the screen scale factor as the Dell XPS 13 has an extremely high resolution
 # resulting in everything being tiny in the VM view
@@ -32,8 +17,7 @@ echo '##########################################################################
 echo '##################### Setting screen resolution ##########################'
 echo ''
 
-changeColour()
-dconf write /com/ubuntu/user-interface/scale-factor "{'Virtual1': 14}"
+gsettings set com.ubuntu.user-interface scale-factor "{'Virtual1': 14}"
 
 echo ''
 echo '##################### Screen resolution set ##############################'
@@ -45,7 +29,6 @@ echo '##########################################################################
 echo '#################### Setting keyboard layout #############################'
 echo ''
 
-changeColour()
 setxkbmap -layout gb
 
 echo ''
@@ -58,7 +41,6 @@ echo '##########################################################################
 echo '################## Installing Sublime Text ###############################'
 echo ''
 
-changeColour()
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
@@ -73,7 +55,6 @@ echo '##########################################################################
 echo '################# Bash set up ############################################'
 echo ''
 
-changeColour()
 cp $DIR/dotbash_profile ~/.bash_profile
 cp $DIR/dobbash_aliases ~/.bash_aliases
 cp $DIR/dotbashrc ~/.bash_rc
@@ -81,6 +62,3 @@ cp $DIR/dotbashrc ~/.bash_rc
 echo ''
 echo '################# Bash is set up #########################################'
 echo '##########################################################################'
-
-
-setterm -term linux -fore 'white'
