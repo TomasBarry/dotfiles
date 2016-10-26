094bd3f02183f9aba2884ed0e7953c1354c05143
Tomas Barry <tomas.barry@hotmail.com>
2016-10-26 13:30:27 +0100

    Install docker-machine

--- debianNodeSetup.sh
+++ debianNodeSetup.sh
@@ -13,3 +13,6 @@ systemctl daemon-reload
 systemctl show --property Environment docker
 systemctl restart docker
 docker run hello-world
+
+curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-` uname -m` >          /usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
+
