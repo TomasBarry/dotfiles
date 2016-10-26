aa5ff7b407b43ba2b5778ffad092c4020a4f25b0
Tomas Barry <tomas.barry@hotmail.com>
2016-10-26 15:34:05 +0100

    Get and source shell completion scripts

--- debianNodeSetup.sh
+++ debianNodeSetup.sh
@@ -16,3 +16,12 @@ docker run hello-world
 
 curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-` uname -m` >          /usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
 
+wget https://raw.githubusercontent.com/docker/machine/master/contrib/completion/bash/docker-machine-prompt.bash -P /etc/bash_completion.d/
+source /etc/bash_completion.d/docker-machine-prompt.bash 
+PS1='[\u@\h \W$(__docker_machine_ps1 " [%s]")]\$ '
+
+wget https://raw.githubusercontent.com/docker/machine/master/contrib/completion/bash/docker-machine-wrapper.bash -P /etc/bash_completion.d/
+source /etc/bash_completion.d/docker-machine-wrapper.bash 
+
+wget https://raw.githubusercontent.com/docker/machine/master/contrib/completion/bash/docker-machine.bash -P /etc/bash_completion.d/
+source /etc/bash_completion.d/docker-machine.bash 
